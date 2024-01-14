78ae3ae0c8eacc8bee41ebec58b7fb8769e0a574
BAE EUNMI <likeyellowand@gmail.com>
2024-01-14 19:28:48 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 104 ms, Memory: 12764 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.next();
+        
+        int inputLen = input.length();
+        
+        System.out.println(inputLen);
+    }
+}
