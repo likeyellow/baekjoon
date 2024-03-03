7f9a2134835e938bc209075a2869fba7bbfd0d61
BAE EUNMI <likeyellowand@gmail.com>
2024-03-03 22:49:59 +0900

    [Silver I] Title: 숨바꼭질, Time: 160 ms, Memory: 21848 KB -BaekjoonHub

---
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+import java.util.Queue;
+import java.util.LinkedList;
+
+class Main {
+    static int n, k;
+    static int[] visited;
+ 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        k = sc.nextInt();
+        visited = new int[100001];
+        
+        Queue<Integer> q = new LinkedList<>();
+        q.add(n);
+        visited[n] = 1;
+        
+        while(!q.isEmpty()) {
+            int now = q.poll();
+            if(now == k) break;
+            int[] next = {now -1, now +1, 2*now};
+            for(int i = 0; i < 3; i++) {
+                if(!isRange(next[i])) continue;
+                
+                if(visited[next[i]] == 0) {
+                    visited[next[i]] = visited[now] + 1;
+                    q.add(next[i]);
+                }
+            }
+        }
+        System.out.println(visited[k] - 1);
+    }
+    static boolean isRange(int x) {
+        return x >= 0 && x <= 100000;
+    }
+}
