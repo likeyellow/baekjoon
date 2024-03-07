ef25a2edaf8fb01e8aeb122f504b47dc2d071876
BAE EUNMI <likeyellowand@gmail.com>
2024-03-08 01:06:51 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 108 ms, Memory: 12852 KB -BaekjoonHub

---
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class Main {
+    static long[] dp = new long[95];
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(fibo1(n));
+    }
+    
+    static long fibo1(int n) {
+        dp[1] = dp[2] = 1;
+        for(int i = 3; i <=n; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
+    }
+}
