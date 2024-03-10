956e28e8c58fc2a73f553178d34c21d404c241a2
BAE EUNMI <likeyellowand@gmail.com>
2024-03-11 00:54:33 +0900

    [Bronze I] Title: 이항 계수 1, Time: 112 ms, Memory: 12840 KB -BaekjoonHub

---
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+class Main {
+    static int n, k;
+    static int[][] dp;
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        k = sc.nextInt();
+        dp = new int[n+1][n+1];
+        for(int i = 0; i <= n; i++) {
+            dp[i][1] = i;
+            dp[i][0] = 1;
+            dp[i][i] = 1;
+        }
+        for(int i = 2; i <= n; i++) {
+            for(int j = 1; j < i; j++) {
+                dp[i][j] = dp[i-1][j] + dp[i-1][j-1];
+            }
+        }
+        System.out.println(dp[n][k]);
+    }
+}
