adee7f81e6fd2460ecb4ae796a38e7cfb90fc47d
BAE EUNMI <likeyellowand@gmail.com>
2024-04-27 12:23:34 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 1.73 ms, Memory: 75 MB -BaekjoonHub

---
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        String[] arr = s.split(" ");
+        
+        for(int i = 0; i < arr.length; i++) {
+            String word = arr[i];
+            
+            if(arr[i].length() == 0) {
+                answer += " ";
+            } else {
+                answer += word.substring(0, 1).toUpperCase();
+                answer += word.substring(1, word.length()).toLowerCase();
+                answer += " ";
+            }
+        }
+        if(s.substring(s.length()-1, s.length()).equals(" ")) {
+            return answer;
+        }
+        return answer.substring(0, answer.length() -1);
+    }
+}
