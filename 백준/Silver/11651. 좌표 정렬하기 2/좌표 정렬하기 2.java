0fa57d70e18245e7911e94a6d9dc7b35f56e1b5d
BAE EUNMI <likeyellowand@gmail.com>
2025-05-18 06:21:08 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 744 ms, Memory: 74132 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -0,0 +1,37 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] line = null;
+        int[][] points;
+        
+        try {
+            int N = Integer.parseInt(br.readLine());
+            points = new int[N][2];
+            
+            for(int i = 0; i < N; i++) {
+                line = br.readLine().split(" ");
+                points[i][0] = Integer.parseInt(line[0]);
+                points[i][1] = Integer.parseInt(line[1]);
+            }
+            Arrays.sort(points, (o1, o2) -> {
+               if(o1[1] == o2[1]) {
+                   return Integer.compare(o1[0], o2[0]);
+               } 
+               return Integer.compare(o1[1], o2[1]);
+            });
+            
+            StringBuilder sb = new StringBuilder();
+            for(int[] point : points) {
+                sb.append(point[0]).append(" ").append(point[1]).append("\n");
+            }
+            System.out.print(sb);
+            
+        }catch(Exception e) {
+            e.printStackTrace();
+        }
+        
+    }
+}
