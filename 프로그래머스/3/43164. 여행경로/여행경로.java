7cfa9e463c675518ddbc1886b3f1bf33530347d5
BAE EUNMI <likeyellowand@gmail.com>
2025-06-06 00:44:00 +0900

    [level 3] Title: 여행경로, Time: 0.59 ms, Memory: 72.8 MB -BaekjoonHub

---
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class Solution {
+    private LinkedList<String> route = new LinkedList<>();
+    
+    public String[] solution(String[][] tickets) {
+        String[] answer = {};
+        //int n = tickets[0].length;
+        
+        Map<String, PriorityQueue<String>> graph = new HashMap<>();
+        
+        for(String[] ticket : tickets) {
+            String from = ticket[0];
+            String to = ticket[1];
+            
+            if(!graph.containsKey(from)) {
+                graph.put(from, new PriorityQueue<>());
+            }
+            graph.get(from).add(to);
+        }
+        dfs("ICN", graph);
+        
+        return route.toArray(new String[0]);
+    }
+    private void dfs(String airport, Map<String, PriorityQueue<String>> graph) {
+         PriorityQueue<String> arrivals = graph.get(airport);
+        while (arrivals != null && !arrivals.isEmpty()) {
+            String next = arrivals.poll();
+            dfs(next, graph);
+        }
+
+        // 후위 순회로 경로 저장
+        route.addFirst(airport);
+    }
+}
